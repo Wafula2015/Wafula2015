d96801b455fea21527cdcd0f020a3d65cbdc2d8a
Wafula2015 <145217943+Wafula2015@users.noreply.github.com>
2023-09-16 19:22:49 +0300

    Create README.m

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @Wafula2015
+- 👀 I’m interested in python and C++
+- 🌱 I’m currently learning ...
+- 💞️ I’m looking to collaborate on various projects 
+- 📫 How to reach me @Sheikh735/Sheikh on Facebook 
+
+<!---
+Wafula2015/Wafula2015 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
